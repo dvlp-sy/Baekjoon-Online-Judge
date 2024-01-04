1950d67de5564fac1741ed50d7f835ff84241b9a
dvlp-sy <sy030404@naver.com>
2024-01-04 17:57:41 +0900

    [add] BOJ_02_14681
    - 사분면 고르기

---
+++ 02_ConditionalStatement/02_14681/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short x, y;
+    cin >> x >> y;
+    
+    if (x>0 && y >0)
+        cout << 1 << '\n';
+    else if (x<0 && y>0)
+        cout << 2 << '\n';
+    else if (x<0 && y<0)
+        cout << 3 << '\n';
+    else
+        cout << 4 << '\n';
+}
