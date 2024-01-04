d706eae9eecf40ffb5d64edeec560e3a44cc9a0f
dvlp-sy <sy030404@naver.com>
2024-01-04 21:41:58 +0900

    [add] BOJ_04_10811
    - 바구니 뒤집기

---
+++ 04_Array/04_10811/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short N, M, i, j;
+    cin >> N >> M;
+    
+    int* arr = new int[N];
+    for (int i=0; i<N; i++)
+        arr[i] = i+1;
+    
+    while (M--)
+    {
+        cin >> i >> j;
+        for (int idx=0; idx<=(j-i)/2; idx++)
+        {
+            int temp = arr[i-1+idx];
+            arr[i-1+idx] = arr[j-1-idx];
+            arr[j-1-idx] = temp;
+        }
+    }
+    
+    for (int i=0; i<N; i++)
+        cout << arr[i] << ' ';
+    cout << '\n';
+}
