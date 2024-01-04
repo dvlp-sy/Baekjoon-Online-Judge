eef60532db46cd50067cab2e8b6f4a03767d4153
dvlp-sy <sy030404@naver.com>
2024-01-04 19:40:28 +0900

    [add] BOJ_03_11021
    - A+B - 7

---
+++ 03_Loop/03_11021/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int T, A, B;
+    std::cin >> T;
+    
+    for (int i=0; i<T; i++)
+    {
+        std::cin >> A >> B;
+        std::cout << "Case #" << i+1 << ": " << A+B << '\n';
+    }
+}
