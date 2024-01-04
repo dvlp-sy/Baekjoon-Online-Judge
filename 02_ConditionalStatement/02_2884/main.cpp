d5915b82d4238e3980e25f643013a3feb5725973
dvlp-sy <sy030404@naver.com>
2024-01-04 18:06:19 +0900

    [add] BOJ_02_2884
    - 알람 시계

---
+++ 02_ConditionalStatement/02_2884/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short H, M;
+    cin >> H >> M;
+    
+    if (M<45)
+    {
+        if (H==0)
+            H =23;
+        else
+            H -= 1;
+            
+        M = 60+(M-45);
+    }
+    else
+        M -= 45;
+    
+    cout << H << ' ' << M << '\n';
+}
