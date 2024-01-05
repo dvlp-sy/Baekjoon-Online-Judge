fe118bc5636571317711dbac61c039f1046b6659
dvlp-sy <sy030404@naver.com>
2024-01-05 12:08:15 +0900

    [add] BOJ_05_10809
    - 알파벳 찾기

---
+++ 05_String/05_10809/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+    
+    int arr[26];
+    for (int i=0; i<26; i++)
+        arr[i] = -1;
+    
+    for (int i=0; i<S.length(); i++)
+    {
+        int idx = S[i]-static_cast<int>('a');
+        if (arr[idx] == -1)
+            arr[idx] = i;
+    }
+    
+    for (int i=0; i<26; i++)
+        cout << arr[i] << ' ';
+    cout << '\n';
+}
