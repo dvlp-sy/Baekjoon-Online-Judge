4a23cded61624390431d14db9a390a5a496d237f
dvlp-sy <sy030404@naver.com>
2024-01-05 12:00:07 +0900

    [add] BOJ_05_11720
    - 숫자의 합

---
+++ 05_String/05_11720/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N, sum=0; string str;
+    cin >> N >> str;
+    for (char c : str)
+        sum += c - static_cast<int>('0');
+    
+    cout << sum << '\n';
+}
