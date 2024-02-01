625e04eb840fd4dd1fbbb878d33c07e5b832d79a
dvlp-sy <sy8389b@gmail.com>
2024-02-01 16:05:10 +0900

    [add] BOJ_12_2798
    - 블랙잭

---
+++ 12_BruteForce/12_2798/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int N, M, elem;
+    cin >> N >> M;
+    
+    int* cards = new int[N];
+    for (int i=0; i<N; i++)
+    {
+        cin >> elem;
+        cards[i] = elem;
+    }
+    
+    int max = 0;
+    for (int i=0; i<N; i++)
+    {
+        for (int j=i+1; j<N; j++)
+        {
+            for (int k=j+1; k<N; k++)
+            {
+                int sum = cards[i] + cards[j] + cards[k];
+                if (sum > max && sum <= M)
+                    max = sum;
+            }
+        }
+    }
+    
+    cout << max << '\n';
+}
